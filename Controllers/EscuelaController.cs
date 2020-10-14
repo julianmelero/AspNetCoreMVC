c337a39d59f0892718d4bd1e63ea335264c177b6
Julián Melero Hidalgo <julian@melerohidalgo.es>
2020-10-14 20:54:51 +0200

    Creating Data

--- Controllers/EscuelaController.cs
+++ Controllers/EscuelaController.cs
@@ -1,30 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using  asp.net_core_MVC.Models;
 using System;
+using System.Linq;
 
 namespace asp.net_core_MVC.Controllers
 {
     public class EscuelaController : Controller
     {
+        private EscuelaContext _context;
+       
         public IActionResult Index() 
         {
-            var escuela = new Escuela();                        
-            escuela.AñoDeCreación = 2005;
-            escuela.UniqueId = Guid.NewGuid().ToString();
-            escuela.Nombre = "Julián's SChool";
-            escuela.Ciudad= "València";
-            escuela.Pais= "España";
-            escuela.Dirección = "C/ Sin número";
+            // var escuela = new Escuela();                        
+            // escuela.AñoDeCreación = 2005;
+            // escuela.UniqueId = Guid.NewGuid().ToString();
+            // escuela.Nombre = "Julián's SChool";
+            // escuela.Ciudad= "València";
+            // escuela.Pais= "España";
+            // escuela.Dirección = "C/ Sin número";
 
             ViewBag.OtrosDatos = "Otros";
+            var escuela = _context.Escuelas.FirstOrDefault();
 
             return View(escuela);
-        }
-
-        public EscuelaController(EscuelaContext context) 
-        {
-            private EscuelaContext _context;
-
+        }      
+         public EscuelaController(EscuelaContext context) 
+        {            
             _context = context;
         }
     }
--- Models/EscuelaContext.cs
+++ Models/EscuelaContext.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.EntityFrameworkCore;
 
 namespace asp.net_core_MVC.Models
@@ -14,6 +15,23 @@ namespace asp.net_core_MVC.Models
         public EscuelaContext (DbContextOptions<EscuelaContext> options): base(options)
         {
             
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            var escuela = new Escuela();                        
+            escuela.AñoDeCreación = 2005;
+            escuela.UniqueId = Guid.NewGuid().ToString();
+            escuela.Nombre = "Julián's SChool";
+            escuela.Ciudad= "València";
+            escuela.Pais= "España";
+            escuela.Dirección = "C/ Sin número";
+
+            modelBuilder.Entity<Escuela>().HasData(
+                escuela
+            );
+
         }
     }
 }
